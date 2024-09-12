59bf165438d755f2ac80cd510e94e91c4e05144a
Jorge Cordero <jorgecorderozx@gmail.com>
2024-09-11 23:44:14 -0400

    Sticky header First Layout for 768px+ resolutions First Layout for 1024px+ resolutions

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let menuIcon = document.querySelector(`img[alt="Menu icon"]`);
 let closeMenuIcon = document.querySelector(`img[alt*="Close"]`);
+let header = document.querySelector("header");
 let navBar = document.querySelector("nav");
 let fadedBackground = document.querySelector(`div[class*=faded]`);
 function toggleMenu(menuState){
     if(menuState){
         menuIcon.style.display = "none";
         closeMenuIcon.style.display = "inline-block";
+        header.style.position = "static"
         navBar.style.display = "inline-block";
         fadedBackground.style.display = "inline-block";
     }
     else {
+        header.style.position = "sticky"
         menuIcon.style.display = "inline-block";
         closeMenuIcon.style.display = "none";
         navBar.style.display = "none";
